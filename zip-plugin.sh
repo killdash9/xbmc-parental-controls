8607590536406e0b9ab90029a2dab3d70a12b03d
killdash9 <killdash9>
2012-07-23 18:05:19 -0600

    shell script for packaging the plugin

---
+++ zip-plugin.sh
@@ -0,0 +1,1 @@
+zip -r dist/script.video.parentalcontrols-`cat script.video.parentalcontrols/addon.xml |grep  'id="script.video.parentalcontrols"' |sed 's,.*version *= *"\([^"]*\)".*,\1,'`.zip script.video.parentalcontrols/
