f56e423fac4377f255704bba80be1f81218f1b60
killdash9 <killdash9>
2012-07-25 00:29:09 -0600

    deleting zip before creating it

--- zip-plugin.sh
+++ zip-plugin.sh
@@ -1,1 +1,2 @@
+rm dist/script.video.parentalcontrols-*
 zip -r dist/script.video.parentalcontrols-`cat script.video.parentalcontrols/addon.xml |grep  'id="script.video.parentalcontrols"' |sed 's,.*version *= *"\([^"]*\)".*,\1,'`.zip script.video.parentalcontrols/
