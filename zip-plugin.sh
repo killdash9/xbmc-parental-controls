bcf29310f1b853b83d4fa2998d90fc28d11b5db3
killdash9 <killdash9>
2012-07-27 21:49:25 -0600

    rev up

--- zip-plugin.sh
+++ zip-plugin.sh
@@ -1,3 +1,4 @@
 FILENAME=script.video.parentalcontrols-`cat script.video.parentalcontrols/addon.xml |grep  'id="script.video.parentalcontrols"' |sed 's,.*version *= *"\([^"]*\)".*,\1,'`.zip
 rm repo/script.video.parentalcontrols/$FILENAME
 zip -r repo/script.video.parentalcontrols/$FILENAME script.video.parentalcontrols/
+python addons_xml_generator.py
