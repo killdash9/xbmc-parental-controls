31d1adfa49b472dfd5592bbb7c5d3c92a9b6ff94
killdash9 <killdash9>
2012-07-27 21:43:22 -0600

    addon repository

--- zip-plugin.sh
+++ zip-plugin.sh
@@ -1,2 +1,3 @@
-rm dist/script.video.parentalcontrols-*
-zip -r dist/script.video.parentalcontrols-`cat script.video.parentalcontrols/addon.xml |grep  'id="script.video.parentalcontrols"' |sed 's,.*version *= *"\([^"]*\)".*,\1,'`.zip script.video.parentalcontrols/
+FILENAME=script.video.parentalcontrols-`cat script.video.parentalcontrols/addon.xml |grep  'id="script.video.parentalcontrols"' |sed 's,.*version *= *"\([^"]*\)".*,\1,'`.zip
+rm repo/script.video.parentalcontrols/$FILENAME
+zip -r repo/script.video.parentalcontrols/$FILENAME script.video.parentalcontrols/
