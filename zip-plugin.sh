f75ec32d7be563a9f75f68bfb47de58a6714f336
killdash9 <killdash9>
2012-07-28 20:03:35 -0600

    removed globalsettings.txt from zip

--- zip-plugin.sh
+++ zip-plugin.sh
@@ -1,5 +1,6 @@
 FILENAME=repo/script.video.parentalcontrols/script.video.parentalcontrols-`cat script.video.parentalcontrols/addon.xml |grep  'id="script.video.parentalcontrols"' |sed 's,.*version *= *"\([^"]*\)".*,\1,'`.zip
 rm $FILENAME
 zip -r $FILENAME script.video.parentalcontrols/
+zip -d $FILENAME script.video.parentalcontrols/globalsettings.txt
 git add $FILENAME
 python addons_xml_generator.py
