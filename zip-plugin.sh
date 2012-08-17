34ac33458d82b507cf295823e16ab5d70dfd5803
killdash9 <killdash9>
2012-08-16 19:08:21 -0600

    rev up

--- zip-plugin.sh
+++ zip-plugin.sh
@@ -1,6 +1,7 @@
 FILENAME=repo/script.video.parentalcontrols/script.video.parentalcontrols-`cat script.video.parentalcontrols/addon.xml |grep  'id="script.video.parentalcontrols"' |sed 's,.*version *= *"\([^"]*\)".*,\1,'`.zip
 rm $FILENAME
 zip -r $FILENAME script.video.parentalcontrols/
-zip -d $FILENAME script.video.parentalcontrols/globalsettings.txt
 git add $FILENAME
 python addons_xml_generator.py
+git status
+echo "Don't forget to push"
