e669ed458953bd402b9a6413a3dbaf7e6920c0eb
killdash9 <killdash9>
2012-08-16 18:58:37 -0600

    actually committing

--- point-to-latest-download.sh
+++ point-to-latest-download.sh
@@ -3,5 +3,5 @@ VERSION=`cat script.video.parentalcontrols/addon.xml |grep  'id="script.video.pa
 echo version is $VERSION &&
 git checkout gh-pages &&
 sed -i "" 's,script.video.parentalcontrols-[^"]*\.zip,script.video.parentalcontrols-'$VERSION'.zip,' index.html &&
-echo git commit index.html -m "pointing to latest download ($VERSION)" &&
+git commit index.html -m "pointing to latest download ($VERSION)" &&
 echo git push 
